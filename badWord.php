63546266089814b25e65d93af0b91b0fa687e2ab
mattiabenelli <benellimattia0@gmail.com>
2023-01-27 17:54:30 +0100

    es done

--- badWord.php
+++ badWord.php
@@ -1,6 +1,7 @@
 <?php 
     $paragraph = $_POST['paragraph'];
     $password = $_POST['password'];
+    $censored = str_replace($password,'***', $password)
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +12,7 @@
     <title>Document</title>
 </head>
 <body>
-    <?php echo $paragraph."".$password; ?>
+    <?php echo $paragraph."".$password; ?> <?php echo strlen($paragraph."".$password); ?>
+    <?php echo $paragraph."".$censored; ?> <?php echo strlen($paragraph."".$censored); ?>
 </body>
 </html>
